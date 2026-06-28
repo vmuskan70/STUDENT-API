ebc50f134b36b58fe17bc15c951f95f235472c70
muskan <vmuskan709@gmail.com>
2026-06-28 18:42:27 +0530

    new

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+app.get("/students/:roll/:name",(req,res)=>{
+    const {roll,name}=req.params;
+    res.json({
+        message:"single student",
+        user_roll:roll,
+        student_name:name
+    });
+});
+app.get("/students",(req,res)=>{
+    res.json({
+        message:"data inside query",
+        data:req.query
+    });
+});
+app.listen(5000,()=>{
+    console.log("serverr started on port 5000")
+})
